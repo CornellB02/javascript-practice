13d08b4fb5a0a011975e4df3527c6288da0a83a8
Cornell Bethea <betheacornellb@gmail.com>
2022-10-06 16:24:43 -0400

    javascript

---
+++ problems.js
@@ -0,0 +1,142 @@
+function divideByThree(num) {
+    /* Returns the passed in number argument divided by three. */
+    return num/3 
+};
+
+function averageOfTwo(num1, num2) {
+    /* Returns the average of two numbers, num1 and num2. */
+    return num1+num2/2
+};
+
+function averageOfFour(num1, num2, num3, num4) {
+    //* Takes in four numbers. The function should return the average of all of
+    return num1+num2+num3+num4/4
+    
+};
+
+function doubler(nums) {
+    /* Takes an array of numbers and returns a new array where every element of
+    the original array is multiplied by 2. */
+    arr = []
+    for (let i = 0; i < nums.length; i++){
+        let num = nums[i]
+        arr.push(num*2)
+    }
+    return arr
+};
+
+console.log(doubler([9,7,67,8,3.9098]))
+
+function combineArrays(arr1, arr2) {
+    /* Takes in two arrays of numbers and returns the two arrays combined into
+    a single array. **Hint**: Use the `Array.concat` method but be aware that
+    calling this method won't permanently change, also known as **mutate**,
+    either array. */
+   return arr1.concat(arr2)
+};
+
+console.log(combineArrays([1,2,3,4],[5,6,7,8]))
+
+function wordWithinArray(word, arr) {
+    /* Takes in both a word and an array of words as arguments and returns a
+    boolean that returns true if that string is located inside of the array, or
+    false if it does not. Use `Array.indexOf`. */
+    for (let i = 0; i < arr.length; i++){
+        if(arr.indexOf(word) === -1){
+            return false 
+        }
+    }
+    return true
+};
+
+console.log(wordWithinArray("hello", ["yes", "no", "hello", "goodbye"]))
+
+function echo(str) {
+    /* Takes in a string and returns that string "echo-ized". E.g. 
+    echo("Mom!"); // => returns "MOM! ... Mom! ... mom!"
+    echo("hey"); // => returns "HEY ... hey ... hey"
+    echo("JUMp"); // => returns "JUMP ... JUMp ... jump" */
+    return str.toUpperCase() + "..." + str.slice(0,str.length - 1).toUpperCase() + str.slice(3).toLowerCase() + "..." + str.toLowerCase()
+
+};
+
+console.log(echo("hype"))
+
+function fizzBuzz(max) {
+    /* Takes a number, max and returns an array that contains every number from
+    0 to max (not inclusive) that is divisible by either 3 or 5, **but not both**. */
+    let arr = []
+    for (let i = 0; i < max; i++){
+        let num = i
+        // console.log(num)
+    }
+};
+
+console.log(90)
+
+function hello(name) {
+    /* Takes in a string name and returns a string saying "Hello, " to that name. */
+    // Your code here
+};
+
+function goodbye(name) {
+    /* Takes in a string name and returns a string saying "Bye, " to that name. */
+    // Your code here
+};
+
+function isFive(num) {
+    /* Takes in a number, num, and returns `true` if a number is equal to 5 and
+    `false` if it is not. */
+    // Your code here
+};
+
+function isOdd(num) {
+    /* Takes in a number and returns `true` if the number is odd and returns
+    `false` otherwise. Try writing this with and without `if` statements */
+    // Your code here
+};
+
+function isSubString(searchString, subString) {
+    /* Takes in two strings, `searchString` and `subString`. Should return
+    `true` if `subString` is a part of the`searchString`, regardless of upper
+    or lower case, and `false` if otherwise. */
+    // Your code here
+};
+
+function aCounter(word) {
+    /* Takes in a word and returns the number of a's within that word. Counts
+    both lowercase (a) and uppercase (A). Your job is to translate the following
+    function to use a `for` loop instead of the `while` loop it is currently
+    using. */
+
+    /* 
+    let index = 0;
+    let count = 0;
+    while (index < word.length) {
+        let char = word[index];
+        if (char === "a" || char === "A") {
+        count += 1;
+        }
+        index++;
+    }
+    return count;
+    */
+    // Your code here
+};
+
+module.exports = {
+    divideByThree,
+    averageOfTwo,
+    averageOfFour,
+    doubler,
+    combineArrays,
+    wordWithinArray,
+    echo,
+    fizzBuzz,
+    hello,
+    goodbye,
+    isFive,
+    isOdd,
+    isSubString,
+    aCounter   
+}
